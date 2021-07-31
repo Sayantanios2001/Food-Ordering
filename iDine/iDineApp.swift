954a39f98ac582911345ee2b36fb8dd6f4ac5f17
Sayantan <samantasayantan23@gmail.com>
2021-07-31 11:53:20 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by sayantan samanta on 31/07/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
